ede4e9fd3bd409edb290a0543c89e41a7a61de8b
Jorge Luis Pérez <jorgeluis700@gmail.com>
2011-04-04 02:22:29 -0300

    Add cromatic calc method with spec examples

--- lib/interval.rb
+++ lib/interval.rb
@@ -1,7 +1,7 @@
 # Esta clase representa un intervalo musical
 class Interval
   DIATONIC_SCALE = ['do', 're', 'mi', 'fa', 'sol', 'la', 'si']
-  
+
   def initialize(note1, note2, step)
     @note1 = note1
     @note2 = note2
@@ -23,7 +23,7 @@ class Interval
     while DIATONIC_SCALE[i] != @note2
       i += 1
       if i > DIATONIC_SCALE.length
-      i = 0; next 
+        i = 0; next 
       end
       count += 1
     end
@@ -31,4 +31,33 @@ class Interval
     count
   end
 
+  # Clasifica el intervalo cromático
+  def cromatic_interval()
+    # inicializa contador e indice de escala
+    i = DIATONIC_SCALE.index(@note1)
+    count = 1
+
+    # si es la misma nota se comienza desde la siguiente
+    if @note1 == @note2
+      count += 1; i += 1
+    end
+  
+    # cuenta las notas
+    while DIATONIC_SCALE[i] != @note2
+      i += 1
+      if i > DIATONIC_SCALE.length
+        i = 0; next 
+      end
+
+      if DIATONIC_SCALE[i] == 'mi' or DIATONIC_SCALE[i] == 'si'
+        count += 1
+      else
+         count += 2
+      end
+    end
+    
+    count = count + (12 * @step) if @step > 0
+    count
+  end
+  
 end
--- spec/interval_spec.rb
+++ spec/interval_spec.rb
@@ -2,14 +2,34 @@ require 'interval'
 
 describe Interval do
   
-  it "el intervalo do-mi deberia ser una tercera" do
+  it "el intervalo diatonico simple do-mi deberia ser una 3ra" do
     i = Interval.new('do', 'mi', 0)
     i.diatonic_interval.should == 3
   end
 
-  it "el intervalo do-do deberia ser una octava" do
+  it "el intervalo diatonico simple do-do deberia ser una 8va" do
     i = Interval.new('do', 'do', 0)
     i.diatonic_interval.should == 8
+  end
+
+  it "el intervalo diatonico compuesto do-mi deberia ser una 10ma" do
+    i = Interval.new('do', 'mi', 1)
+    i.diatonic_interval.should == 10
+  end
+
+  it "el intervalo cromatico simple do-mi deberia ser de 4 semi-tonos" do
+    i = Interval.new('do', 'mi', 0)
+    i.cromatic_interval.should == 4
+  end
+
+  it "el intervalo cromatico simple do-do deberia ser de 12 semi-tonos" do
+    i = Interval.new('do', 'do', 0)
+    i.cromatic_interval.should == 12
+  end
+
+  it "el intervalo cromatico compuesto do-mi deberia ser de 16 semi-tonos" do
+    i = Interval.new('do', 'mi', 1)
+    i.cromatic_interval.should == 16
   end
 
 end
