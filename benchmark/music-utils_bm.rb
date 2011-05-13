1af1d461f608e5b233e264bd2bf63eca6d3e57cb
Jorge Luis Pérez <jorgeluis700@gmail.com>
2011-05-13 01:31:54 -0300

    Tidy up benchmarks output

--- benchmark/music-utils_bm.rb
+++ benchmark/music-utils_bm.rb
@@ -7,24 +7,33 @@ require 'benchmark'
 n = 100_000
 label_width = 60
 
+# Helper method to print benchmarks titles 
+def title(desc, n)
+  puts '-' * 105
+  puts desc + ' ' + n.to_s + ' times: '
+end
+
+# Benchmarks
+
+title 'Classifying do-mi interval', n
 Benchmark.bm(label_width) do |b|
-  b.report('Classifying do-mi interval with strings (number):') do 
+  b.report('with strings (number):') do 
     n.times { MusicUtils.number('do', 'mi') }
   end
-  b.report('Classifying do-mi interval with strings (quality):') do 
+  b.report('with strings (quality):') do 
     n.times { MusicUtils.quality('do', 'mi') }
   end
-  b.report('Classifying do-mi interval with strings (short notation):') do 
+  b.report('with strings (short notation):') do 
     n.times { MusicUtils.short('do', 'mi') }
   end
   
-  b.report('Classifying do-mi interval with scales (number):') do
+  b.report('with scales (number):') do
     n.times { MusicUtils.number(Scales::DO, Scales::MI) }
   end
-  b.report('Classifying do-mi interval with scales (quality):') do
+  b.report('with scales (quality):') do
     n.times { MusicUtils.quality(Scales::DO, Scales::MI) }
   end
-  b.report('Classifying do-mi interval with scales (short notation):') do
+  b.report('with scales (short notation):') do
     n.times { MusicUtils.short(Scales::DO, Scales::MI) }
   end
 end
